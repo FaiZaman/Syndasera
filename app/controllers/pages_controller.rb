02a4f5e1a00a500f5d352c3486d6b31f41ffa87f
Faiz <faiz-uz.zaman@dur.ac.uk>
2020-07-30 16:47:30 +0100

    Created pages controller and updated routes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def make_request
+  end
+end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get pages_home_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get pages_about_url
+    assert_response :success
+  end
+
+  test "should get make_request" do
+    get pages_make_request_url
+    assert_response :success
+  end
+
+end
