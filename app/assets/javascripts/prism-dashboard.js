f3aa66221b94a63b64b9f71900fdc07c62e6795b
rebecafiadeiro <rebeca.fiadeiro@hotmail.com>
2020-08-11 12:00:52 +0100

    d3 update

--- app/assets/javascripts/prism-dashboard.js
+++ app/assets/javascripts/prism-dashboard.js
@@ -1,3 +1,59 @@
+console.log("hello world")
+var loadData = function(){
+                $.ajax({
+                  type: 'GET',
+                  contentType: 'application/json; charset=utf-8',
+                  url: '/present_data',
+                  dataType: 'json',
+                  success: function(data){
+                    visualisation(data);
+                  },
+                  failure: function(result){
+                    error();
+                  }
+                });
+              };
+function error() {
+    console.log("Something went wrong!");
+}
+
+function visualisation(data) {
+  console.log(data);
+
+//<meta charset="utf-8">
+//<script src="https://d3js.org/d3.v4.js"></script>
+//<div id="my_first_histogram"></div>
+
+//  <script>
+    var margin = {top: 10, right: 30, bottom: 30, left: 40},
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+    var svg = d3.select("#my_first_histogram")
+      .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform",
+              "translate(" + margin.left + "," + margin.top + ")");
+
+// X axis: scale and draw:
+      var x = d3.scaleLinear()
+        .domain([0, 1000])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+        .range([0, width]);
+      svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+      var histogram = d3.histogram()
+          .value(function(data) { return data.isisisisis; })   // I need to give the vector of value
+          .domain(x.domain())  // then the domain of the graphic
+              .thresholds(x.ticks(70)); // then the numbers of bins
+//    </script>
+
+
+}
+
 d3.select("body").transition().style("background-color", "#f6abb650")
 
 d3.select("#content").append("div").attr("class", "container")
@@ -5,3 +61,7 @@ d3.select("#content").append("div").attr("class", "container")
                     For the time being please use dummy data defined as an object in this file. \
                     Once we get the rails model with the data in it I might become a .js.erb so you\
                     can query the model from here :D.")
+
+$(document).ready(function(){ 
+loadData()
+ });
