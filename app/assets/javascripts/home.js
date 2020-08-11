7c14bcedeaf1b1167f7b13c2990b4a60825ef2d2
Faiz <faiz-uz.zaman@dur.ac.uk>
2020-08-11 11:38:48 +0100

    Deleted development.sqlite3; added filtering functionality to column list

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();   
+    
+    $('[data-toggle="tooltip"]').tooltip(); // PRISM description
+
+    // select all and none for column checkboxes
+    $("#select-all").on('click', function(){
+        $(".checkbox").prop('checked', true)
+    });
+
+    $("#select-none").on('click', function(){
+        $(".checkbox").prop('checked', false)
+    });
+
 });
