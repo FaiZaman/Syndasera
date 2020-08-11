a33f5e19266d46bcad25e304475d69aa6bd5da6c
rebecafiadeiro <rebeca.fiadeiro@hotmail.com>
2020-08-11 14:54:18 +0100

    d3 update

--- app/assets/javascripts/prism-dashboard.js
+++ app/assets/javascripts/prism-dashboard.js
@@ -40,17 +40,42 @@ function visualisation(data) {
               "translate(" + margin.left + "," + margin.top + ")");
 
 // X axis: scale and draw:
-      var x = d3.scaleLinear()
-        .domain([0, 1000])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+    var x = d3.scaleLinear()
+        .domain([0, 20])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+//        .domain([0, d3.max(data, function(d) { return +d["Hemoglobin (g/dL) [EUPATH_0000047]"]})])
         .range([0, width]);
-      svg.append("g")
+    svg.append("g")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x));
 
-      var histogram = d3.histogram()
-          .value(function(data) { return data.Particpant_Id; })   // I need to give the vector of value
-          .domain(x.domain())  // then the domain of the graphic
-              .thresholds(x.ticks(70)); // then the numbers of bins
+    var histogram = d3.histogram()
+        .value(function(data) { return data["Hemoglobin (g/dL) [EUPATH_0000047]"]; })   // I need to give the vector of value
+        .domain(x.domain())  // then the domain of the graphic
+        .thresholds(x.ticks(20)); // then the numbers of bins
+
+                // And apply this function to data to get the bins
+    var bins = histogram(data);
+
+                  // Y axis: scale and draw:
+    var y = d3.scaleLinear()
+        .range([height, 0]);
+        y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+                  // append the bar rectangles to the svg element
+    svg.selectAll("rect")
+        .data(bins)
+        .enter()
+        .append("rect")
+            .attr("x", 1)
+            .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+            .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
+            .attr("height", function(d) { return height - y(d.length); })
+            .style("fill", "#69b3a2")
+
+                  //});
+
 //    </script>
 
 
