c70f9779bc4a5d1fdb880e50a528bd32b353886d
Noo Rashbass <nathaniel.rashbass@phe.gov.uk>
2020-08-25 13:37:12 +0100

    removed the pink texty bit from dashboard

--- app/assets/javascripts/prism-dashboard.js
+++ app/assets/javascripts/prism-dashboard.js
@@ -622,13 +622,13 @@ autocorr("#autocorrelation", auto_visits_df)
 autocorr("#autocorrelation2", auto_malaria_df)
 }
 
-d3.select("body").transition().style("background-color", "#f6abb650")
-
-d3.select("#content").append("div").attr("class", "container")
-.append("p").text("Hello, welcome to the d3 playground. Everything should be possible in here <3.\
-                    For the time being please use dummy data defined as an object in this file. \
-                    Once we get the rails model with the data in it I might become a .js.erb so you\
-                    can query the model from here :D.")
+// d3.select("body").transition().style("background-color", "#f6abb650")
+//
+// d3.select("#content").append("div").attr("class", "container")
+// .append("p").text("Hello, welcome to the d3 playground. Everything should be possible in here <3.\
+//                     For the time being please use dummy data defined as an object in this file. \
+//                     Once we get the rails model with the data in it I might become a .js.erb so you\
+//                     can query the model from here :D.")
 
 $(document).ready(function(){ 
 loadData()
